1d920f1ed1be940e23c9f52d6e8f4c2361b1ab02
Francisco <fvcneto.master98@gmail.com>
2020-08-15 19:07:57 -0300

    draw random squares

--- main.py
+++ main.py
@@ -9,31 +9,50 @@ import pygame
 WIDTH, HEIGHT = 640, 480
 FPS = 60
 
+
 ## colors
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+GRAY = (75, 75, 75)
 RED = (255,0,0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-COLORS = [WHITE, BLACK, RED, GREEN, BLUE]
 
 ## game
 TILE_SIZE = 32
+T_WIDTH = WIDTH // TILE_SIZE
+T_HEIGHT = HEIGHT // TILE_SIZE
+GAME_COLORS = [RED, GREEN, BLUE]
 
 ##############################################################################
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-grid = True
 
-def render():
+## game
+grid = True
+matrix = []
+# store random square colors
+for x in range(0, WIDTH, TILE_SIZE):
+    for y in range(0, HEIGHT, TILE_SIZE):
+        color = random.choice(GAME_COLORS)
+        matrix.append((screen, color, (x, y, TILE_SIZE, TILE_SIZE)))
+
+def draw_grid():
     if grid:
         for x in range(0, WIDTH, TILE_SIZE):
-            pygame.draw.line(screen, WHITE, (x, 0), (x, HEIGHT))
+            pygame.draw.line(screen, GRAY, (x, 0), (x, HEIGHT))
         for y in range(0, HEIGHT, TILE_SIZE):
-            pygame.draw.line(screen, WHITE, (0, y), (WIDTH, y))
-    # draw random square colors
+            pygame.draw.line(screen, GRAY, (0, y), (WIDTH, y))
 
+def render():
+    # draw random square colors
+    for i in range(T_WIDTH):
+        for j in range(T_HEIGHT):
+            pygame.draw.rect(*matrix[i + j * T_WIDTH])
+
+    # grid
+    draw_grid()
     
 
 def update():
