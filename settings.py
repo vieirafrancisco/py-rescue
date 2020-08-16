1a5239f40da98586f4d98ae55fbf4d287cabcb05
Francisco <fvcneto.master98@gmail.com>
2020-08-16 13:17:28 -0300

    create settings file to store the constants values

---
+++ settings.py
@@ -0,0 +1,17 @@
+## window
+WIDTH, HEIGHT = 640, 480
+FPS = 60
+
+## colors
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GRAY = (75, 75, 75)
+RED = (255,0,0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+## game
+TILE_SIZE = 32
+T_WIDTH = WIDTH // TILE_SIZE
+T_HEIGHT = HEIGHT // TILE_SIZE
+GAME_COLORS = [RED, GREEN, BLUE]
